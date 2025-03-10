ba9de5a0c4ac4b1c81a732861ffdfba29c976e1a
Daniel_mest <danieloppongboahx@gmail.com>
2025-03-10 16:02:38 +0000

    Created Server

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import mongoose from "mongoose";
+
+//create an express app
+const app = express();
+
+//Database connection
+await mongoose.connect(process.env.MONGO_URI).then(()=>{
+    console.log("Connected to MongoDB");
+}).catch((err)=>{
+    console.log(err)
+})
+
+//Server is listening
+app.listen(4000, ()=>{
+    console.log("Server is running on port 4000");
+})
