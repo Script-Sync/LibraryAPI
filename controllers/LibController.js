11d047d65f19a30d5f607b1654e838489eae154f
Daniel_mest <danieloppongboahx@gmail.com>
2025-03-11 22:01:41 +0000

    Merge 1.0

---
+++ controllers/LibController.js
@@ -0,0 +1,71 @@
+import { LibraryModel } from '../models/LibModel.js';
+import { addBookDetails } from "../validators/LibValidator.js";
+
+// Add a new book
+export const addBook = async (req, res, next) => {
+  try {
+    const { title, author, publishedYear } = req.body;
+    const createbook = new LibraryModel({ title, author, publishedYear });
+    await createbook.save();
+    res.status(201).json({ message: "Book added" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get all books
+export const getAllBooks = async (req, res, next) => {
+  try {
+    const getBooks = await LibraryModel.find();
+    res.status(200).json(getBooks);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get a book by ID
+export const getBookById = async (req, res, next) => {
+  try {
+    const book = await LibraryModel.findById(req.params.id);
+    if (book) {
+      res.status(200).json(book);
+    } else {
+      res.status(404).json({ message: "Book not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Update a book
+export const updateBook = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const { title, author, publishedYear } = req.body;
+    const updateDetail = await LibraryModel.findByIdAndUpdate(
+      id,
+      { title, author, publishedYear },
+      { new: true }
+    );
+    if (updateDetail) {
+      res.status(200).json({ message: "Book updated", updateDetail });
+    } else {
+      res.status(404).json({ message: "Book not found" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Delete a book
+export const deleteBook = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    await LibraryModel.findByIdAndDelete(id);
+    res.status(200).json({
+      message: "Book deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
