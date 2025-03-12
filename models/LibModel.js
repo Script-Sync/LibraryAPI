7b810b0329b3f68b14f5e1cd9b0e3a05d7fdfe00
Lisa <lisa.baiden26@gmail.com>
2025-03-12 19:18:01 +0000

    added the new models to the schema

--- models/LibModel.js
+++ models/LibModel.js
@@ -6,6 +6,15 @@ const librarySchema = new Schema(
     author: { type: String, required: true },
     publishedYear: { type: Number, required: true },
     image: { type: String, required: true },
+    genres: {type:[String], required:true},
+    description: {type: String, required:true},
+    publisher: {type:String, required:true},
+    rating:{type: Number, min:0, max:5},
+    reviews: [{
+      user: {type:String, required:true},
+      comment:{type:String, required:true},
+      date:{type: Date, default:Date.now}
+    }]
   },
   {
     timestamps: true,
